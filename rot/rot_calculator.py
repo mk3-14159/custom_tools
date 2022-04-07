00d7d6b81e16136a2c186cff1356b141607aed55
mk3-14159 <evanxmk@gmail.com>
2022-04-07 18:13:36 +0100

    rot

---
+++ rot/rot_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import binascii
+import codecs
+import argparse
+
+def rot(phrase):
+  decoded = codecs.decode(phrase, 'rot_13')  
+  return decoded
+
+def main():
+  parser = argparse.ArgumentParser(description='decode rot13 algorithm')
+  parser.add_argument('-phrase', '--phrase', action='store', dest='phrase', help='Decode rot13 passphrase')
+  args = parser.parse_args()
+  phrase = args.phrase
+  print(rot(phrase))
+ 
+if __name__ == "__main__":
+  main()
+  
